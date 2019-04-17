0c6f129a8405e4b943e4392586ebfd593c5af194
magicdawn <magicdawn@qq.com>
2019-04-17 22:22:14 +0800

    test: switch to mocha, jest has issues with request/request-promise

--- test/simple.js
+++ test/simple.js
@@ -1,6 +1,7 @@
 const dl = require('..')
 const {getSize, needDownload, download} = dl
 const fs = require('fs-extra')
+const should = require('should')
 
 const url = 'https://www.baidu.com/img/bd_logo1.png'
 const file = __dirname + '/../example-files/bd_logo1.png'
@@ -8,39 +9,40 @@ const redo = () => dl({url, file, skipExists: false})
 
 it('download works', async function() {
   const ret = await redo()
-  expect(ret).toEqual({skip: false})
-  expect(fs.existsSync(file)).toBe(true)
+  ret.should.eql({skip: false})
+  fs.existsSync(file).should.ok()
 
   const ret2 = await dl({url, file, useChromeUa: false})
-  expect(ret2).toEqual({skip: true})
-  expect(fs.existsSync(file)).toBe(true)
+  ret2.should.eql({skip: true})
+  fs.existsSync(file).should.ok()
 })
 
 it('getSize', async function() {
   await redo()
   const size = await getSize(url)
   const realsize = fs.statSync(file).size
-  expect(size).toBe(realsize)
+  size.should.equal(realsize)
 })
 
 describe('needDownload', function() {
   it('when skipExists = false', async function() {
-    expect(await needDownload({skipExists: false})).toBe(true)
+    const ret = await needDownload({skipExists: false})
+    ret.should.ok()
   })
 
   it('when file not exists', async () => {
     await fs.unlink(file)
-    expect(await needDownload({url, file})).toBe(true)
+    ;(await needDownload({url, file})).should.ok()
   })
 
   it('it works', async function() {
     await redo()
-    expect(await needDownload({url, file, skipExists: true})).toBe(false)
+    ;(await needDownload({url, file, skipExists: true})).should.equal(false)
   })
 })
 
 describe('download', function() {
   it('fail when timeout', async () => {
-    return expect(download({url: 'bad-url', file})).rejects.toThrow('bad-url')
+    download({url: 'bad-url', file}).should.rejectedWith({message: /bad-url/})
   })
 })
