d4677c92706e282c70179cf104a12da2c2563723
magicdawn <magicdawn@qq.com>
2022-06-29 22:35:33 +0800

    chore: jest

--- test/read-url.ts
+++ test/read-url.ts
@@ -4,13 +4,13 @@ describe('readUrl', () => {
   it('readUrl works', async () => {
     {
       const str = await readUrl({ url: 'https://www.baidu.com', encoding: 'utf8' })
-      str.should.match(/百度/)
+      expect(str).toMatch(/百度/)
     }
 
     {
       const buf = await readUrl({ url: 'https://www.baidu.com' })
-      buf.should.instanceOf(Buffer)
-      buf.toString().should.match(/百度/)
+      expect(buf).toBeInstanceOf(Buffer)
+      expect(buf.toString()).toMatch(/百度/)
     }
   })
 })
